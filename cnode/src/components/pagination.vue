43cc0e6c02d9931c17d62aa07b137acdb8570279
ggg50 <xjfletitgo@gmail.com>
2019-07-23 12:02:18 +0800

    create pagination

--- cnode/src/components/pagination.vue
+++ cnode/src/components/pagination.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="paginationWrapper">
-    <button>首页</button>
-    <button>下一页</button>
+    <button @click="changeBtn('首页')">首页</button>
+    <button @click="changeBtn('上一页')">上一页</button>
     <button v-if="jduge">......</button>
-    <button v-for="pageNumber in pages" @click="changebtn(pageNumber)">{{pageNumber}}</button>
-    <button>下一页</button>
+    <button v-for="pageNumber in pages" @click="changeBtn(pageNumber)" :class="[{currentPage: pageNumber === currentPage}, 'pageBtn']">{{pageNumber}}</button>
+    <button @click="changeBtn('下一页')">下一页</button>
   </div>
 </template>
 
 <script>
+import $ from "jquery";
 
 export default {
   name: "Pagination",
@@ -20,13 +21,37 @@ export default {
     }
   },
   methods: {
-    changebtn(pageNumber){
-      if(pageNumber === this.pages[4]){
-        this.pages.shift();
-        this.pages.splice(4, 0, this.pages[3] + 1);
-      } else if (pageNumber === this.pages[0] && pageNumber !== 1){
-        this.pages.unshift(this.pages[0] - 1);
-        this.pages.splice(5, 1);
+    changeBtn(pageContent){
+
+      if(typeof pageContent !== "number") {
+        if(pageContent === "首页") {
+          this.pages = [1,2,3,4,5,'......'];
+          this.changeBtn(1);
+        } else if (pageContent === "上一页"){
+          $("button.currentPage").prev().click();
+          return;
+        } else if (pageContent === "下一页"){
+          $("button.currentPage").next().click();
+          return;
+        }
+
+      } else {
+
+        this.currentPage = pageContent;
+
+        if(pageContent > 4){
+          this.jduge = true;
+        } else {
+          this.jduge = false;
+        };
+
+        if(pageContent === this.pages[4]){
+          this.pages.shift();
+          this.pages.splice(4, 0, this.pages[3] + 1);
+        } else if (pageContent === this.pages[0] && pageContent !== 1){
+          this.pages.unshift(this.pages[0] - 1);
+          this.pages.splice(5, 1);
+        }
       }
     }
   }
